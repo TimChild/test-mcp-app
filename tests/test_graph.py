e5425dacb5d4c5f89fa458593ea6029839014b00
Tim Child <timjchild@gmail.com>
2025-03-31 11:06:28 -0700

    add test that tools bound

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,8 +1,12 @@
 import uuid
-from typing import Any
+from typing import Any, Callable, Iterator, Literal, Optional, Sequence, Union
 
 import pytest
-from langchain_core.runnables import RunnableConfig
+from langchain_core.language_models import LanguageModelInput
+from langchain_core.language_models.fake_chat_models import FakeMessagesListChatModel
+from langchain_core.messages import AIMessage, BaseMessage
+from langchain_core.runnables import Runnable, RunnableConfig
+from langchain_core.tools import BaseTool
 from langgraph.graph.graph import CompiledGraph
 from langgraph.store.base import Item
 from langgraph.store.memory import InMemoryStore
@@ -13,8 +17,39 @@ from host_app.graph_runner import GraphRunner
 from host_app.models import GraphUpdate, UpdateTypes
 
 
+class FakeChatModel(FakeMessagesListChatModel):
+    # NOTE: list-list in-case it's called multiple times
+    tools_bound: list[list[BaseTool]] = []
+
+    def bind_tools(
+        self,
+        tools: Sequence[dict[str, Any] | type | Callable | BaseTool],
+        *,
+        tool_choice: Optional[Union[str, Literal["any"]]] = None,
+        **kwargs: Any,
+    ) -> Runnable[LanguageModelInput, BaseMessage]:
+        _, _ = tool_choice, kwargs
+        base_tools: list[BaseTool] = []
+        for tool in tools:
+            assert isinstance(tool, BaseTool)
+            base_tools.append(tool)
+        self.tools_bound.append(base_tools)
+        return self
+
+
+@pytest.fixture
+def fake_chat_model() -> FakeChatModel:
+    return FakeChatModel(
+        responses=[
+            AIMessage("First response"),
+            AIMessage("Second response"),
+            AIMessage("Third response"),
+        ]
+    )
+
+
 @pytest.fixture(autouse=True)
-def container() -> Application:
+def container(fake_chat_model: FakeChatModel) -> Iterator[Application]:
     container = Application()
     container.config.from_yaml("config.yml")
     container.wire(
@@ -23,7 +58,8 @@ def container() -> Application:
             "host_app.graph_runner",
         ]
     )
-    return container
+    with container.llms.main_model.override(fake_chat_model):
+        yield container
 
 
 @pytest.fixture
@@ -86,3 +122,16 @@ async def test_memory_store_standalone():
     after = await store.aget(namespace=("testing",), key="test")
     assert isinstance(after, Item)
     assert after.value == {"value": "value"}
+
+
+async def test_graph_binds_tools(
+    graph_runner: GraphRunner, fake_chat_model: FakeChatModel, basic_runnable_config: RunnableConfig
+):
+    _ = await graph_runner.graph.ainvoke(
+        input=InputState(question="Hello"), config=basic_runnable_config
+    )
+
+    assert len(fake_chat_model.tools_bound) == 1, "Should bind tools once"
+    assert fake_chat_model.tools_bound[0][0].name == "test-tool", (
+        "Should bind test-tool during tests"
+    )
